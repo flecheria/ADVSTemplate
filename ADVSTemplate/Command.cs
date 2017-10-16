160fd589cce87a2fe7ab429ae5452daedc6eb0a5
flecheria <flecheria@yahoo.it>
2017-10-16 17:04:03 +0200

    added all the available files

---
+++ ADVSTemplate/Command.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.AdvanceSteel.Runtime;
+using Autodesk.AdvanceSteel.DocumentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AdvanceSteel.CADAccess;
+using Autodesk.AdvanceSteel.Profiles;
+using Autodesk.AdvanceSteel.Geometry;
+using Autodesk.AdvanceSteel.Modelling;
+
+// using alias directive for AdvanceSteel
+using ASTransactionManager = Autodesk.AdvanceSteel.CADAccess.TransactionManager;
+using ASObjectId = Autodesk.AdvanceSteel.CADLink.Database.ObjectId;
+
+// using alias directive for Autocad
+
+namespace ADVSTemplate
+{
+    class Command
+    {
+        [CommandMethodAttribute("TEST_GROUP", "CreateElements", "CreateElements",
+                                CommandFlags.Modal | CommandFlags.UsePickSet | CommandFlags.Redraw)]
+        public void Create()
+        {
+            DocumentManager.LockCurrentDocument();
+            Transaction t = ASTransactionManager.StartTransaction();
+
+            // your code here
+
+            t.Commit();
+            DocumentManager.UnlockCurrentDocument();
+        }
+    }
+}
---
+++ ADVSTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Resources;
+using Autodesk.AdvanceSteel.Runtime;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Addin")]
+[assembly: AssemblyDescription("A template to create Autodesk AdvanceSteel Addins")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Maffeis Engineering s.p.a.")]
+[assembly: AssemblyProduct("Addin")]
+[assembly: AssemblyCopyright("Copyright © Maffeis Engineering s.p.a. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2a4bb17-9cd3-469e-b481-5034ce819df0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// AdvanceSteel specification
+[assembly: ExtensionApplicationAttribute(typeof(ADVSTemplate.Addin))]
+[assembly: CommandClassAttribute(typeof(ADVSTemplate.Command))]
